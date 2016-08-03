2be88424a57fa95a325538c0b819ac27279078c8
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-08-03 19:36:24 -0400

    set up comment routes

---
+++ routes/comments.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/comments/:postID', getCommentsForAPost);
+router.post('/comments', createComment);
+router.delete('/comments/:commentID', deleteComment);
+router.put('/comments/:commentId', updateComment);
+
+module.exports = router;
+
+function getCommentsForAPost(req, res, next){
+  console.log('getting all comments');
+  next();
+}
+
+function createComment(req, res, next){
+  console.log('Creating comment');
+  next();
+}
+
+function deleteComment(req, res, next){
+  console.log('deleting comment');
+  next();
+}
+
+function updateComment(req, res, next){
+  console.log('updating comment');
+  next();
+}
